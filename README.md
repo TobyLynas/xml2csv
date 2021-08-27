ad1666efbe41df08f00e20d1742952e62f4bddd9
Toby Lynas <tobylynas@tobys-air.lan>
2021-08-27 10:46:40 +0100

    Finish inital working

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # xml2csv
 
-Takes a xml file and converts into a csv format. B
+Takes a xml file and converts into a csv format.
 Built using Node.js.
+
+
+# To run, ensure node is on your enviroment and the use "node index.js"
--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-console.log("----- XML2CSV -----");
+fs = require("fs");
+const parser = require("xml2json");
+const ObjectsToCsv = require("objects-to-csv");
+//Imports needed modules for converting and parsing files
 
-fs = require('fs');
-var parser = require('xml2json');
+const fileName = `./PO - In Boot`;
+//Change file name to change file converted, could be written into a loop to iterate through multiple files at once
 
-fs.readFile( './PO - In Boot.xml', function(err, data) {
-    var json = parser.toJson(data);
-    let dataString = JSON.stringify(json);
-    for (let i = 0; i < dataString.length; i++){
-        if (dataString[i] == "Something to filter out here!" ) {
-            dataString = dataString.slice(i);
-        }
-    };
-    console.log(dataString);
- });
+const xmlType = ".xml";
+const csvType = ".csv"
+
+fs.readFile(fileName.concat(xmlType), function (err, data) {
+  var json = parser.toJson(data);
+  //Parses raw xml into a JSON format
+  const obj = JSON.parse(json);
+  //Parses JSON into a object to be used by JS
+  //Headers and other data can be accessed using this object, not sure if they are needed?
+
+  (async () => {
+    const csv = new ObjectsToCsv(obj.Purchase_Order.Purchase_Order_Lines);
+
+    // Save to file:
+    await csv.toDisk(fileName.concat(csvType));
+
+    // Return the CSV file as string:
+    console.log(await csv.toString());
+  })();
+});
